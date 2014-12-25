10351578b95e293d57cc34d48057a37725db16ec
srikanth <srikanth.iit@gmail.com>
2014-12-25 12:40:37 +0530

    Chapter 4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Ruby on Rails Sample App"
   end
 
   test "should get help" do
